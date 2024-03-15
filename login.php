680a974d523c4f240447737a5c60a084201f2245
MiltonVenegas <101526482+MiltonVenegas@users.noreply.github.com>
2024-03-14 20:23:24 -0600

    Sesion

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+if(isset($_SESSION["user"])){
+    $_SESSION["user"] = $_POST["Usuario"];
+}
+
+$_SESSION["user"] = $_POST["usuario"];
+
+echo'<h2>'.$_POST["usuario"].'<h2><br>';
+
+echo'<h2>'.$_SESSION["user"].'<h2>';
+
+session_regenerate_id();
+$_SESSION["id"] = session_id();
+
+echo '<h1>'.$_SESSION["id"].'<h1>';
+
+echo '<a href="logout.php" type="button" title="Log Out" name="Ingresar">LogOut</a>';
